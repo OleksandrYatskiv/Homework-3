0d40576ebc7581a536bf6c372763dcaed5e16af0
Oleksandr Yatskiv <yatsk1660@gmail.com>
2023-04-22 19:16:58 +0200

    Finished work

---
+++ index.js
@@ -0,0 +1,12 @@
+firstNumber = parseFloat(prompt("Enter a number:"));
+secondNumber = parseFloat(prompt("Enter another number:"));
+
+sumResult = firstNumber + secondNumber;
+minusResult = firstNumber - secondNumber;
+multipleResult = firstNumber * secondNumber;
+devideResult = firstNumber / secondNumber;
+
+alert(`${firstNumber} + ${secondNumber} = ${sumResult} 
+${firstNumber} - ${secondNumber} = ${minusResult}
+${firstNumber} * ${secondNumber} = ${multipleResult}
+${firstNumber} / ${secondNumber} = ${devideResult}`);
